f2376afab0baf62a0f9c0778bf8c2781d5c74141
Андрей <zhdanovandrey14.03.2008@gmail.com>
2026-07-31 20:31:52 +0300

    удалил часть кода

--- mai.c
+++ mai.c
@@ -3,20 +3,5 @@
     int b;
     int main(void)
     {
-        scanf("%d",&a);
-        scanf("%d",&b);
-        if (a>b)
-        {
-        printf("Первое число больше\n");
-        }
-        else if (a<b)
-        {
-            printf("Второе число больше\n");
-        }
-        else
-        {
-        printf("Числа равны\n");
-        }
 
-            return 0;
     }
